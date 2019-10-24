ed8653f3aabaf6d709e01d0422fe85970ff8c745
ydahi <ymdahi@gmail.com>
2019-10-24 13:47:57 -0400

    test

--- scripts/modelviewer.js
+++ scripts/modelviewer.js
@@ -38,9 +38,48 @@ H5P.ModelViewer = (function ($, THREE) {
 
     // var stl_viewer = new StlViewer(document.getElementById("stl_cont"), { models: [ {id:0, filename:filepath} ] });
 
-    var container = document.createElement( 'div' );
-    document.body.appendChild( container );
-    scene = new THREE.Scene();
+    var scene = new THREE.Scene();
+
+// Create a basic perspective camera
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 4;
+
+// Create a renderer with Antialiasing
+var renderer = new THREE.WebGLRenderer({antialias:true});
+
+// Configure renderer clear color
+renderer.setClearColor("#000000");
+
+// Configure renderer size
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+// Append Renderer to DOM
+document.body.appendChild( renderer.domElement );
+
+// ------------------------------------------------
+// FUN STARTS HERE
+// ------------------------------------------------
+
+// Create a Cube Mesh with basic material
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: "#433F81" } );
+var cube = new THREE.Mesh( geometry, material );
+
+// Add cube to Scene
+scene.add( cube );
+
+// Render Loop
+var render = function () {
+  requestAnimationFrame( render );
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  // Render the scene
+  renderer.render(scene, camera);
+};
+
+render();
 
     
 
@@ -92,4 +131,4 @@ H5P.ModelViewer = (function ($, THREE) {
   };
  
   return C;
-})(H5P.jQuery);
+})(H5P.jQuery, H5P.ThreeJS);
